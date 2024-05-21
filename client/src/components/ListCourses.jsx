4cb398498314a9f2327595ffdfdef1ede408395b
Saad-byte-star <itssaadtariq@gmail.com>
2024-05-21 21:44:29 +0500

    Solved the Error For the Course Render on the Home and Enrolled Courses Nav Tabs

--- client/src/components/ListCourses.jsx
+++ client/src/components/ListCourses.jsx
@@ -1,28 +1,12 @@
 import { Container, Row, Col, Button, Card } from "react-bootstrap";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams , useLocation } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function ListCourses(){
   const { uid } = useParams();
   const [courses , setCourses ] = useState([]);
-
-//   const fetchCourses = async () => {
-//     let url = `http://localhost:8000/api/courses`;
-//     try{
-//     const response = await fetch(url, {
-//       method: "GET",
-//     });
-//     if (response.ok) {
-//       const data = await response.json();
-//       setCourses(data);
-//     }
-//     }catch(error){ 
-//     console.log(`failed to fetch courses error : ${error}`)
-//   }
-// }
-// console.log(courses);
-// fetchCourses();
-
+  const location = useLocation();
+  console.log(location);
 useEffect(() => {
   const fetchCourses = async () => {
     let url = `http://localhost:8000/api/courses`;
@@ -57,7 +41,9 @@ courses.map( (course) => {
 return (
     <Container>
       <Row className="my-4">
-        {enrolledCourses.map((course) => (
+        { location.pathname === "/home" 
+        ? 
+        courses.map((course) => (
           <Col key={course._id} className="col-4 my-3">
             <Card className="rounded-0 m-0 w-100">
               <Card.Img className="rounded-0" variant="top" src="/imgs/1.png" />
@@ -72,7 +58,25 @@ return (
               </Card.Body>
             </Card>
           </Col>
-        ))}
+        )) 
+        :
+        enrolledCourses.map((course) => (
+          <Col key={course._id} className="col-4 my-3">
+            <Card className="rounded-0 m-0 w-100">
+              <Card.Img className="rounded-0" variant="top" src="/imgs/1.png" />
+              <Card.Body>
+                <Card.Title>{course.Title}</Card.Title>
+                <Card.Text>{course.Description}</Card.Text>
+                <Link>
+                <Button value={course._id} className="float-end rounded-0" variant="dark">
+                  Enroll Now
+                </Button>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        )) 
+        }
       </Row>
     </Container>
   );
--- client/src/services/globals.jsx
+++ client/src/services/globals.jsx
@@ -3,14 +3,7 @@ import React, { createContext, useEffect,  useState } from 'react';
 export const UserContext = createContext();
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState({
-    "_id": "",
-    "Name": "",
-    "Email": "",
-    "Password": "",
-    "Role": "",
-    "__v": ""
-  }); // Initial state can be an empty object, null, or any default value
+  const [user, setUser] = useState(); // Initial state can be an empty object, null, or any default value
 
   useEffect(() => {
     // Load user from localStorage if available
